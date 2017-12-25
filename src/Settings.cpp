e61206dd72a6c2d09dfd635bae79095e901c9d7f
Helder Garcia <helder.garcia@gmail.com>
2017-12-25 17:34:00 -0200

    Assign remoteNode default.

--- src/Settings.cpp
+++ src/Settings.cpp
@@ -61,7 +61,7 @@ void Settings::load() {
     }
 
     if (!m_settings.contains(OPTION_CONNECTION)) {
-         m_connectionMode = "auto";
+         m_connectionMode = "remote";
     }
 
     if (!m_settings.contains(OPTION_DAEMON_PORT)) {
@@ -233,7 +233,7 @@ QString Settings::getConnection() const {
         connection = m_settings.value(OPTION_CONNECTION).toString();
     }
     else {
-    connection = "auto"; // default
+        connection = "remote"; // default
     }
     return connection;
 }
@@ -259,6 +259,8 @@ QString Settings::getCurrentRemoteNode() const {
     QString remotenode;
     if (m_settings.contains(OPTION_REMOTE_NODE)) {
         remotenode = m_settings.value(OPTION_REMOTE_NODE).toString();
+    } else {
+        remotenode = "35.225.224.17:8314";
     }
     return remotenode;
 }
